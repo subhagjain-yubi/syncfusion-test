005b1134982506630ef35119710cae4935fd1986
subhagcredavenue <subhag.jain@credavenue.com>
2023-02-28 13:22:30 +0530

    commmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
-import { SpreadsheetComponent } from '@syncfusion/ej2-react-spreadsheet';
-import React from 'react';
+import { SpreadsheetComponent ,Spreadsheet} from '@syncfusion/ej2-react-spreadsheet';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import {  getFormalatedValue} from './utlils';
+import {  getSampleFile,getFormalatedValue} from './utlils';
 
 function App() {
-  // console.log(getSampleFile());
-  console.log(getFormalatedValue());
+  const [spreadsheet,setSpreadsheet]=useState()
+  useEffect(()=>{
+    fetch("https://js.syncfusion.com/demos/ejservices/data/Spreadsheet/LargeData.xlsx") // fetch the remote url
+    .then((response) => {
+    response.blob().then((fileBlob) => {
+        var file = new File([fileBlob], "Sample.xlsx"); //convert the blob into file
+        // this.spreadsheet.open({ file: file }); // open the file into Spreadsheet
+        // setSpreadsheet({ file: file })
+    });
+});
+// }
+  },[])
+  console.log(getSampleFile());
+  // console.log(getFormalatedValue());
+//   let spreadsheet: Spreadsheet = new Spreadsheet({
+//     openUrl: 'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open',
+//     saveUrl: 'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save',
+//     created: (): void => {
+//         fetch("http://localhost:8080/api/v1/getSampleFile") // fetch the remote url
+//             .then((response) => {
+//               console.log(response);
+//                 response.blob().then((fileBlob) => { // convert the excel file to blob
+//                 let file = new File([fileBlob], "Sample.xlsx"); //convert the blob into file
+//                 spreadsheet.open({ file: file }); // open the file into Spreadsheet
+//                 })
+//             })
+//     }
+// });
+//Render the initialized Spreadsheet
+// spreadsheet.appendTo('#spreadsheet');
+
+
+   
+  // return (<SpreadsheetComponent  ref={(ssObj) => { spreadsheet = ssObj; }} openUrl='https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open' created={created.bind()}>
+  //                   </SpreadsheetComponent>);
+    
   
   return (
     <div className="App">
-    <SpreadsheetComponent  allowSave={true} allowOpen={true} openUrl='https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open'/>
+      <div id="spreadsheet"></div>
+    <SpreadsheetComponent allowSave={true} allowOpen={true} openUrl='http://localhost:8080/api/v1/getSampleFile'/>
    </div>
    
   );
--- src/utlils.tsx
+++ src/utlils.tsx
@@ -1,27 +1,23 @@
 
-// export const getSampleFile = () => {
-//     fetch(`http://localhost:8080/api/v1/getSampleFile`, {
-//       method: "GET",
-//       headers: {
-//         "Content-type": "APPLICATION_OCTET_STREAM",
-//         "responseType": 'blob'
-//       },
+export const getSampleFile = () => {
+    fetch(`http://localhost:8080/api/v1/getSampleFile`, {
+      method: "GET"
      
-//     }).then((res) => {
-//         console.log(res);
-//         const blob = new Blob([res.blob as BlobPart], {
-//             type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-//           });
-//           console.log(blob);
-//         var file = new File([blob], "name");
-//         console.log(file);
-//         // const element = document.createElement('a');
-//         // element.href = URL.createObjectURL(file);
-//         // element.setAttribute('download', ".xlsx");
-//         // document.body.appendChild(element);
-//         // element.click();
-//     });
-// };
+    }).then((res) => {
+        console.log(res.body);
+        // const blob = new Blob([res.blob as BlobPart], {
+        //     type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        //   });
+        //   console.log(blob);
+        // var file = new File([blob], "name");
+        // console.log(file);
+        // const element = document.createElement('a');
+        // element.href = URL.createObjectURL(file);
+        // element.setAttribute('download', ".xlsx");
+        // document.body.appendChild(element);
+        // element.click();
+    });
+};
 
 export const sendFile=() => {
     fetch(`http://localhost:8080/api/v1/file`, {
